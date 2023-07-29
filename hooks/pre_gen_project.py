20b4d4ef9e53a6e72eb38df9a7ee2aa56b20c17a
ajanderson1 <ajanderson1@gmail.com>
2023-07-29 19:12:23 +0200

    ..

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -17,3 +17,35 @@ if not re.match(MODULE_REGEX, MODULE_NAME):
 
     # Exit to cancel project
     sys.exit(1)
+
+
+def get_service_account_email(credentials_file_path):
+    with open(credentials_file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    if 'client_email' in data:
+        return data['client_email']
+    else:
+        raise ValueError("Invalid credentials file. Could not find 'client_email' field.")
+
+# Path to the credentials JSON file provided by the user during Cookiecutter instantiation.
+credentials_file_path = '{{ cookiecutter.path_to_gcp_credentials }}'
+gcp_service_account_email = get_service_account_email(credentials_file_path)
+
+
+import ast
+from collections import OrderedDict
+
+# Assuming cookiecutter_config is a string containing the ordered dictionary representation
+# e.g., cookiecutter_config = "OrderedDict([('full_name', 'AJ Anderson'), ...])"
+
+# Convert the string to an OrderedDict
+cookiecutter_config = "{{ cookiecutter }}"
+
+raise ValueError(cookiecutter_config)
+
+cookiecutter_config_ordered_dict = ast.literal_eval(cookiecutter_config)
+
+# Save the updated Cookiecutter configuration back to the file.
+with open('cookiecutter.json', 'w') as config_file:
+    json.dump(cookiecutter_config, config_file)
