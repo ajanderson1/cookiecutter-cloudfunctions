5d2419d2e1fba7410bff6e5264909c7dfd330142
ajanderson1 <ajanderson1@gmail.com>
2023-07-29 19:08:58 +0200

    ...

--- {{cookiecutter.project_slug}}/terraform/function.tf
+++ {{cookiecutter.project_slug}}/terraform/function.tf
@@ -2,13 +2,13 @@ resource "google_cloudfunctions_function" "function" {
   name                  = "{{cookiecutter.function_entry_point}}"
   description           = "{{cookiecutter.function_description}}"
   runtime               =  "python310"
-  {% if cookiecutter.function_trigger == "http" %}
+  {% if cookiecutter.function_trigger_type == "http" %}
   function_entry_point           = "{{cookiecutter.function_entry_point}}"
-  {% elif cookiecutter.function_trigger == "cloud storage" %}
+  {% elif cookiecutter.function_trigger_type == "cloud storage" %}
   function_entry_point           = "{{cookiecutter.function_entry_point}}"
-  {% elif cookiecutter.function_trigger == "pub/sub" %}
+  {% elif cookiecutter.function_trigger_type == "pub/sub" %}
   function_entry_point           = "{{cookiecutter.function_entry_point}}"
-  {% elif cookiecutter.function_trigger == "firestore" %}
+  {% elif cookiecutter.function_trigger_type == "firestore" %}
   function_entry_point           = "{{cookiecutter.function_entry_point}}"
   {% endif %}
   source_archive_bucket = google_storage_bucket.bucket.name
@@ -18,7 +18,7 @@ resource "google_cloudfunctions_function" "function" {
   service_account_email = google_service_account.sa.email
   available_memory_mb   = {{cookiecutter.function_memory}}
 
-  {% if cookiecutter.function_trigger == "http" %}
+  {% if cookiecutter.function_trigger_type == "http" %}
   trigger_http = true
   {% endif %}
 }
