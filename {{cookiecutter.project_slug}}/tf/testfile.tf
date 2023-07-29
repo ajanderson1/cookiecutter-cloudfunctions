79d5d3fd66c3cd22f1bf2ceffa72e6247d943593
ajanderson1 <ajanderson1@gmail.com>
2023-07-29 17:02:10 +0200

    ..

--- {{cookiecutter.project_slug}}/tf/testfile.tf
+++ {{cookiecutter.project_slug}}/tf/testfile.tf
@@ -1,5 +1,6 @@
 # tempary test file
 {{cookiecutter.project_slug}}_testfile = "testfile"
-# this is now containing latest changes made by {{cookiecutter.full_name}}
+# this is now containing latest changes made by {{cookiecutter.full_name}}\
+# The prj name is : {{cookiecutter.project_name}}
 
 
