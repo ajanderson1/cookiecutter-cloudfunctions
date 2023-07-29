36051fd565019f4a8c8204fd246daa63fa56d43f
ajanderson1 <ajanderson1@gmail.com>
2023-07-29 19:36:20 +0200

    ...

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -19,45 +19,40 @@ if not re.match(MODULE_REGEX, MODULE_NAME):
     sys.exit(1)
 
 
-def get_service_account_email(credentials_file_path):
-    with open(credentials_file_path, 'r') as json_file:
-        data = json.load(json_file)
 
-    if 'client_email' in data:
-        return data['client_email']
-    else:
-        raise ValueError("Invalid credentials file. Could not find 'client_email' field.")
+# DOES NOT WORK - PROBLEM: cannot save to the cookiecutter OrderedDict and therefore cannot pass vars to template files
+# def get_service_account_email(credentials_file_path):
+#     with open(credentials_file_path, 'r') as json_file:
+#         data = json.load(json_file)
 
-# Path to the credentials JSON file provided by the user during Cookiecutter instantiation.
-credentials_file_path = '{{ cookiecutter.path_to_gcp_credentials }}'
-gcp_service_account_email = get_service_account_email(credentials_file_path)
+#     if 'client_email' in data:
+#         return data['client_email']
+#     else:
+#         raise ValueError("Invalid credentials file. Could not find 'client_email' field.")
+
+# # Path to the credentials JSON file provided by the user during Cookiecutter instantiation.
+# credentials_file_path = '{{ cookiecutter.path_to_gcp_credentials }}'
+# gcp_service_account_email = get_service_account_email(credentials_file_path)
 
 
-import ast
-from collections import OrderedDict
+# import ast
+# from collections import OrderedDict
 
-# Assuming cookiecutter_config is a string containing the ordered dictionary representation
-# e.g., cookiecutter_config = "OrderedDict([('full_name', 'AJ Anderson'), ...])"
+# # Assuming cookiecutter_config is a string containing the ordered dictionary representation
+# # e.g., cookiecutter_config = "OrderedDict([('full_name', 'AJ Anderson'), ...])"
 
-# Convert the string to an OrderedDict
-cookiecutter_config = "{{ cookiecutter }}"
+# # Convert the string to an OrderedDict
+# cookiecutter_config = "{{ cookiecutter }}"
 
-# use literal_eval to safely parse the string into an actual OrderedDict object
-dict_str = cookiecutter_config.replace("OrderedDict", "")
-cookiecutter_config_ordered_dict = ast.literal_eval(dict_str)
-cookiecutter_config_dict = dict(cookiecutter_config_ordered_dict)
+# # use literal_eval to safely parse the string into an actual OrderedDict object
+# dict_str = cookiecutter_config.replace("OrderedDict", "")
+# cookiecutter_config_ordered_dict = ast.literal_eval(dict_str)
+# cookiecutter_config_dict = dict(cookiecutter_config_ordered_dict)
 
-# add the service account email to the cookiecutter config
-cookiecutter_config_dict['gcp_service_account_email'] = gcp_service_account_email
-
-from cookiecutter.main import cookiecutter
-
-cookiecutter(
-    cookiecutter_config_dict['project_slug'],
-    no_input=True,
-    extra_context={'gcp_service_account_email': gcp_service_account_email}
-)
+# # add the service account email to the cookiecutter config
+# cookiecutter_config_dict['gcp_service_account_email'] = gcp_service_account_email
 
 # # Save the updated Cookiecutter configuration back to the file.
 # with open('cookiecutter.json', 'w') as config_file:
 #     json.dump(cookiecutter_config_dict, config_file)
+
