b2c3e46247c65fd56a9d6b6a1735c06c4628802f
ajanderson1 <ajanderson1@gmail.com>
2023-07-29 19:06:33 +0200

    ..

--- {{cookiecutter.project_slug}}/terraform/function.tf
+++ {{cookiecutter.project_slug}}/terraform/function.tf
@@ -2,18 +2,6 @@ resource "google_cloudfunctions_function" "function" {
   name                  = "{{cookiecutter.function_entry_point}}"
   description           = "{{cookiecutter.function_description}}"
   runtime               =  "python310"
-# Code for custom runtimes
-#   {% if cookiecutter.gcp_python_runtime == "3.11" %}
-#   runtime               = "python311"
-#   {% elif cookiecutter.gcp_python_runtime == "3.10" %}
-#   runtime               = "python310"
-#   {% elif cookiecutter.gcp_python_runtime == "3.9" %}
-#   runtime               = "python39"
-#   {% elif cookiecutter.gcp_python_runtime == "3.8" %}
-#   runtime               = "python38"
-#   {% elif cookiecutter.gcp_python_runtime == "3.7" %}
-#   runtime               = "python37"
-#   {% endif %}
   {% if cookiecutter.function_trigger == "http" %}
   function_entry_point           = "{{cookiecutter.function_entry_point}}"
   {% elif cookiecutter.function_trigger == "cloud storage" %}
