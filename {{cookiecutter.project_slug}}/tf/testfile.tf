964806d1e5af1d2d422af7d3537a3073c0dc4a41
ajanderson1 <ajanderson1@gmail.com>
2023-07-29 16:47:16 +0200

    ..

--- {{cookiecutter.project_slug}}/tf/testfile.tf
+++ {{cookiecutter.project_slug}}/tf/testfile.tf
@@ -1,4 +1,10 @@
 # tempary test file
 {{cookiecutter.project_slug}}_testfile = "testfile"
+{{cookiecutter.project_slug}}_testfile = "testfile"
+{{cookiecutter.project_slug}}_testfile = "testfile"
+{{cookiecutter.project_slug}}_testfile = "testfile"
+{{cookiecutter.project_slug}}_testfile = "testfile"
+{{cookiecutter.project_slug}}_testfile = "testfile"
+{{cookiecutter.project_slug}}_testfile = "testfile"
 
 
